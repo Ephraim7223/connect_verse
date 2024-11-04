5d16e7c65d7123d665e384ba1a0187e46a4c88d6
Ephraim7223 <basseyephraim55@gmail.com>
2024-11-04 12:39:27 +0100

    added new functionalities

--- SRC/utils/globalFn.js
+++ SRC/utils/globalFn.js
@@ -1,4 +1,8 @@
 import cryptoHash from 'crypto'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
 export function hashValue(value) {
     const hash = cryptoHash.createHash('sha256')
     hash.update(value)
@@ -7,4 +11,12 @@ export function hashValue(value) {
 
 export function comparePassword(inputPassword, hashedPassword) {
     return hashValue(inputPassword) === hashedPassword
+}
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn : '3d'
+    })
+
+    return token
 }
